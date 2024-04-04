9b0f7ad52900ab6233c9432aa2fb83b72a1f905e
Saksham Singh <sakshamsingh.93502@gmail.com>
2024-04-04 23:54:25 +0530

    Commiting

--- adbw.ps1
+++ adbw.ps1
@@ -1,1 +1,11 @@
-adb connect "device_static_ip":$(nmap "device_static_ip" -p 30000-60000 | Where-Object{$_ -match "tcp open"} | ForEach-Object {$_.split("/")[0]})
+adb disconnect 
+$null = adb connect "Your_device_IP":$(nmap "Your_device_IP" -p 30000-60000 -open | Where-Object{$_ -match "tcp open"} | ForEach-Object {$_.split("/")[0]})
+$null
+# Check if the device is connected through wireless adb
+$adbDevices = adb devices | Select-Object -Skip 1 -First 1
+if ($adbDevices -match 'device') {
+    Write-Output "Device is connected through wireless adb"
+} else {
+    Write-Output "Device is not connected through wireless adb"
+}
+
