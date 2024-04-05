f62182fb4aeda3ad46f304881f127c0d38c272c9
Saksham Singh <sakshamsingh.93502@gmail.com>
2024-04-05 22:53:14 +0530

    Change the port range

--- adbw.ps1
+++ adbw.ps1
@@ -1,5 +1,5 @@
 adb disconnect 
-$null = adb connect "Your_device_IP":$(nmap "Your_device_IP" -p 30000-60000 -open | Where-Object{$_ -match "tcp open"} | ForEach-Object {$_.split("/")[0]})
+$null = adb connect 192.168.1.100:$(nmap 192.168.1.100 -p 30000-50000 -open | Where-Object{$_ -match "tcp open"} | ForEach-Object {$_.split("/")[0]})
 $null
 # Check if the device is connected through wireless adb
 $adbDevices = adb devices | Select-Object -Skip 1 -First 1
